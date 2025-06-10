0d21d73f650d39756d4294ef315599ca11c73fba
aashath0317 <89287795+aashath0317@users.noreply.github.com>
2025-06-10 09:52:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -34,4 +34,4 @@ def latest_data():
     return jsonify({"message": "No data received yet"}), 404
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=80)
